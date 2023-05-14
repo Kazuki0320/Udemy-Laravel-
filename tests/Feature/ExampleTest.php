b9492f890df67a62157c778e154be7e14aa23f1e
Kazuki.T <107445735+Kazuki0320@users.noreply.github.com>
2023-05-14 21:57:33 +0900

    setUpを利用した準備

--- tests/Feature/ExampleTest.php
+++ tests/Feature/ExampleTest.php
@@ -7,6 +7,16 @@ use Tests\TestCase;
 
 class ExampleTest extends TestCase
 {
+    public $user;
+
+    protected function setUp(): void
+    {
+        //各テストが呼ばれる前に呼び出される
+        parent::setUp();
+
+        $this->user = 'hoge'; 
+    }
+
     /**
      * A basic test example.
      *
@@ -14,8 +24,17 @@ class ExampleTest extends TestCase
      */
     public function test_the_application_returns_a_successful_response()
     {
-        $response = $this->get('/');
 
+        dump($this->user);
+        $response = $this->get('/');
+        $response->assertStatus(200);
+    }
+
+    /** @test */
+    public function test_the_application_returns_a_successful_response1()
+    {
+        dump($this->user);
+        $response = $this->get('/');
         $response->assertStatus(200);
     }
 }
