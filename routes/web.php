3e455c86437c9f9162fd999db8294507856f4aa5
Kazuki.T <107445735+Kazuki0320@users.noreply.github.com>
2023-05-28 11:40:07 +0900

    追加分

--- routes/web.php
+++ routes/web.php
@@ -5,16 +5,16 @@ use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\TestController;
 use App\Http\Controllers\ContactFormController;
 
-/*
-|--------------------------------------------------------------------------
-| Web Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register web routes for your application. These
-| routes are loaded by the RouteServiceProvider and all of them will
-| be assigned to the "web" middleware group. Make something great!
-|
-*/
+// /*
+// |--------------------------------------------------------------------------
+// | Web Routes
+// |--------------------------------------------------------------------------
+// |
+// | Here is where you can register web routes for your application. These
+// | routes are loaded by the RouteServiceProvider and all of them will
+// | be assigned to the "web" middleware group. Make something great!
+// |
+// */
 Route::get('tests/test', [ TestController::class, 'index']);
 
 // Route::resource('contacts', ContactFormController::class);
